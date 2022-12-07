c7a22c7d4b3c3b9045dc3095d4972de3fbb8c5f7
luigibreda <luigimatheus@hotmail.com>
2022-12-06 23:40:02 -0300

    algumas pequenas mudanças

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-import stknICO from "./stknICO.json";
+import stknICO from "./StknICO.json";
 
 export const stakingICOAddress = "0x6770aeB9A40d202C8F301dc6F8A145e3D18A5607";
 export const stakingICOAbi = stknICO.abi;
