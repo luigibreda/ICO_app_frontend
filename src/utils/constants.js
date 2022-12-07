fc459048ea0d27dc15cb64ca2db25e8058f274a3
luigibreda <luigimatheus@hotmail.com>
2022-12-07 18:56:00 -0300

    resolvendo erro build vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-import stknICO from "./stknICO.json";
+import stknICO from "./StknICO.json";
 
 export const stakingICOAddress = "0x10DCEF238433A7a4330146A7EFDbe4DF41DD43c3";
 export const stakingICOAbi = stknICO.abi;
