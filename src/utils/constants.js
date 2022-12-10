7fc26b67ead0c709b757413002abe272f415404a
luigibreda <luigimatheus@hotmail.com>
2022-12-09 21:40:26 -0300

    voltando

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import stknICO from "./WVWICO.json";
 
-export const stakingICOAddress = "0x4f4520E70dCbc05777D8877ff856Ea41c2efA682";
+export const stakingICOAddress = "0x4B75a6ff226Eb8d1b0b6262794D5a9538c6ba0D5";
 export const stakingICOAbi = stknICO.abi;
